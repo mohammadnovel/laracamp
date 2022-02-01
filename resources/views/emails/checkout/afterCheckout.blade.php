b92fcc7565e51974186debf5276dd63fd85961a3
mohramdhani <velrest48@gmail.com>
2022-02-02 00:06:08 +0700

    check dasboard

--- resources/views/emails/checkout/afterCheckout.blade.php
+++ resources/views/emails/checkout/afterCheckout.blade.php
@@ -5,8 +5,9 @@ Hi , {{$checkout->User->name}}
 <br>
 Thank you for register on <b>{{$checkout->Camp->title}}</b>, please see payment instruction bu click the button below.
 
-@component('mail::button', ['url' => route('user.checkout.invoice', $checkout->id)])
-Get Invoice
+{{-- @component('mail::button', ['url' => route('user.checkout.invoice', $checkout->id)]) --}}
+@component('mail::button', ['url' => route('dashboard')])
+Check My Dashboard
 @endcomponent
 
 Thanks,<br>
