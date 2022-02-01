6b460f04cb40e235b4f725c762f3479f7d873dba
mohramdhani <velrest48@gmail.com>
2022-02-01 16:52:00 +0700

    validation checkout

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\Controller;
 use App\Models\Checkout;
 use Illuminate\Http\Request;
+
+use App\Http\Requests\User\Checkout\Store;
 use App\Models\Camp;
 use Auth;
 class CheckoutController extends Controller
@@ -41,7 +43,7 @@ class CheckoutController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request, Camp $camp)
+    public function store(Store $request, Camp $camp)
     {
         $data = $request->all();
         $data['user_id'] = Auth::id();
